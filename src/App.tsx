9f70cea10887c36036788a1ccd19594663cbf74b
Raymond <rrupiah@gmail.com>
2023-12-06 22:06:23 +0000

    project setup and packages

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+	return (
+		<div>
+			<h1 className='text-3xl font-bold text-red-600 underline text-center'>Hello world!</h1>
+		</div>
+	);
+}
+
+export default App;
